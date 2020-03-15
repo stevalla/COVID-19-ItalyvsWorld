044056dfa161e76cc0a4b9244550868e46ba91a9
Andrea Aquilino <aquilinoa@prometeia.lan>
2020-03-15 14:27:42 +0100

    script per grafici mondiali, working directory dinamica

--- scripts/r/graph.r
+++ scripts/r/graph.r
@@ -1,10 +1,25 @@
 
 library(lubridate)
+library(data.table)
+library(ggplot2)
+library(rstudioapi)
 
-world_death <- read.csv("C:\\Users\\aquilinoa\\Desktop\\COVID-19-ItalyvsWorld\\data\\world\\history\\time_series_19-covid-Deaths.csv")
-world_confirmed <- read.csv("C:\\Users\\aquilinoa\\Desktop\\COVID-19-ItalyvsWorld\\data\\world\\history\\time_series_19-covid-Confirmed.csv")
-world_recovered <- read.csv("C:\\Users\\aquilinoa\\Desktop\\COVID-19-ItalyvsWorld\\data\\world\\history\\time_series_19-covid-Recovered.csv")
+#creo path ai file con directory dinamica
+setwd("./../..")
+file_path_temp <- "data/world/history/time_series_19-covid-"
+data_csv <- paste(Sys.Date()-1,".csv",sep="")
+death_f <- paste(file_path_temp,"Deaths_",data_csv,sep="")
+confirmed_f <- paste(file_path_temp,"Confirmed_",data_csv,sep="")
+recovered_f <- paste(file_path_temp,"Recovered_",data_csv,sep="")
+path_final_deaths<- file.path(getwd(),death_f)
+path_final_confirmed<- file.path(getwd(),confirmed_f)
+path_final_recovered<- file.path(getwd(),recovered_f)
+world_death <- read.csv(path_final_deaths)
+world_confirmed <- read.csv(path_final_confirmed)
+world_recovered <- read.csv(path_final_recovered)
 
+
+#inizio a creare il mio dataframe
 province <- world_death[,1]
 country <- world_death[,2]
 #droppo le prime 4 colonne
@@ -36,24 +51,62 @@ recovered<-unlist(recovered)
 #tabella finale
 new_table <- data.frame(datario,countries,provinces,death,confirmed,recovered)
 
+################## GRAFICI PER PAESE ############################
 
 
-library(data.table)
-library(ggplot2)
 #creo il data table
 new_table <- setDT(new_table)
-italy <- new_table[countries == "Italy"]
-#scalo i confirmed per 10
-italy <- cbind(italy[,1],italy[,4],italy[,5]/10,italy[,6])#preparo il dataset per ggplot
-italy <-melt(italy,"datario")
-#plot con asse secondario *
-setwd("C:\\Users\\aquilinoa\\Desktop\\COVID-19-ItalyvsWorld\\results\\graph")
-file_name <- paste("Italy_",format(today()-1, "%Y-%m-%d"), ".pdf", sep = "")
-pdf(file_name,height=4,paper='special')
-plot_italy<-ggplot(italy,aes(datario,value,color=variable,group=variable))+geom_line()+ggtitle("Covid 19 Italy")
-plot_italy <- plot_italy + scale_y_continuous("death - recovered", sec.axis = sec_axis(~.*10, name="confirmed"))
-#salvo il grafico in un pdfn
-plot_italy
-dev.off()
+names <- unique(new_table$countries)
+#c="US"
+k<-0
 
+table_final <-c()
+
+for (c in names){
+death <- c()
+confirmed <- c()
+recovered <- c()
+areas <- c()
+k<-k+1
+world <- new_table[countries == c]
+f<-0
+#i="2020-01-23"
+  for (i in unique(world$datario)){
+    f<-f+1
+    world_new <- world[datario == i]
+    death[f] <- sum(world_new[,4])
+    confirmed[f] <-  sum(world_new[,5])
+    recovered[f] <- sum(world_new[,6])
+    areas[f] <- c
+  
+  }
+
+  table <- data.frame(areas,death,confirmed,recovered)
+  table_final <- rbind(table_final,table)
+}
+
+dates <- rep(days,length(unique(table_final$areas)))
+table_final <- setDT(cbind.data.frame(dates,table_final))
+
+path_temp<-"results/graph"
+final_wd <- file.path(getwd(),path_temp)
+setwd(final_wd)
+file_name <- paste("World_",format(today()-1, "%Y-%m-%d"), ".pdf", sep = "")
+z<-0
+# apro la connessione
+pdf(file_name,height=4,paper='special')
+for (i in unique(table_final$areas)){
+  z <- z+1
+  final_plot <- table_final[areas == i]
+  final <-melt(final_plot,c("dates","areas"))
+  
+  plot_world <- ggplot(final,aes(dates,value,color=variable,group=variable))+geom_line()+ggtitle(paste("Covid 19",i,sep=" - "))
+  
+  #plot_world <- plot_world + scale_y_continuous("death - recovered", sec.axis = sec_axis(~.*10, name="confirmed"))
+  
+   print(plot_world)
+  
+}
+dev.off()
+  
 
